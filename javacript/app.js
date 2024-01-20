6bd1365021acf454ecbeb3dd9263e75bfab519a7
Hady Bah <hadymbah@gmail.com>
2024-01-20 05:42:11 -0500

    OCR TesseractJS

--- javacript/app.js
+++ javacript/app.js
@@ -1,4 +1,4 @@
 const T = require("tesseract.js")
 
-T.recognize('./ocrtest1.png', 'eng', {logger: e => console.log(e)})
-    .then(out => console.log(out.data.text))
+T.recognize('./ocrtest1.png', 'eng', {logger: e => console.log(e)})//path to image, language, log to show progress
+    .then(out => console.log(out.data.text))//returns promise, log out data specifically text 
