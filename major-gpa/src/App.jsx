6039c4b062c4bf24a20eae309100c05658e02801
Hady Bah <hadymbah@gmail.com>
2024-01-22 02:24:24 -0500

    calculating gpa

--- major-gpa/src/App.jsx
+++ major-gpa/src/App.jsx
@@ -13,11 +13,13 @@ function App() {
   //transfer selection
   // const [mockData, setMockData] = useState([]);
   const [targetKeys, setTargetKeys] = useState([]);
+  const [transferredCourses, setTransferredCourses] = useState([]);
+  const [calculatedGPA, setCalculatedGPA] = useState(null);
 
   const filterOption = (inputValue, option) =>
     option.description.indexOf(inputValue) > -1 || option.title.indexOf(inputValue) > -1;
 
-  const handleChange = (newTargetKeys) => {
+  const handleChange = (newTargetKeys, direction, moveKeys) => {
     setTargetKeys(newTargetKeys);
   };
 
@@ -25,8 +27,50 @@ function App() {
     console.log('search:', dir, value);
   };
 
+  const calculateGPA = () => {
+    // Retrieve exact grades and credits from the JSON data for the transferred courses
+    const updatedTransferredCourses = mockData.filter(course => targetKeys.includes(course.key));
+    setTransferredCourses(updatedTransferredCourses);
 
-  //uploading image
+    // Calculate GPA based on the exact grade and credit information from transferred courses
+    const totalCredits = updatedTransferredCourses.reduce((sum, course) => sum + course.credits, 0);
+    const totalGradePoints = updatedTransferredCourses.reduce((sum, course) => sum + convertGradeToPoints(course.grade), 0);
+    const calculatedGPA = totalGradePoints / totalCredits;
+    setCalculatedGPA(calculatedGPA.toFixed(2));
+  };
+
+  const convertGradeToPoints = (grade) => {
+    switch (grade) {
+      case 'A':
+        return 4.0;
+      case 'A-':
+        return 3.7;
+      case 'B+':
+        return 3.3;
+      case 'B':
+        return 3.0;
+      case 'B-':
+        return 2.7;
+      case 'C+':
+        return 2.3;
+      case 'C':
+        return 2.0;
+      case 'C-':
+        return 1.7;
+      case 'D+':
+        return 1.3;
+      case 'D':
+        return 1.0;
+      case 'D-':
+        return 0.7;
+      case 'F':
+        return 0.0;
+      default:
+        return 0.0;
+    }
+  };
+  
+
   const props = {
     beforeUpload: (file) => {
       const isPNG = file.type === 'image/png';
@@ -38,7 +82,7 @@ function App() {
     onChange: (info) => {
       console.log(info.fileList);
     },
-  };  
+  }; 
 
 
 
@@ -59,7 +103,7 @@ function App() {
         <p>Upload an image of your transcript</p>
       </div>
       <div className='center-title'>
-      <Upload {...props}>
+      <Upload {...props} maxCount={1}>
       <Button icon={<UploadOutlined />}>Upload png only</Button>
       </Upload>
       </div>
@@ -71,7 +115,10 @@ function App() {
         <h1>Step 2</h1>
       </div>
       <div className='center-title'>
-        <p>Transfer all major related</p>
+        <p><strong>Transfer all major related courses</strong></p>
+      </div>
+      <div className='center-title'>
+        <p>Add below if needed or innacurate</p>
       </div>
       <div className='center-directions'>
       <p>
@@ -98,13 +145,24 @@ function App() {
       <Divider/>
       
       <div className='center-title'>
-        <h1>Almost There!</h1>
+        <h1>One more click!</h1>
       </div>
       <div className='center-title'>
-        <p>Button to calculate</p>
+        <Button type="primary" onClick={calculateGPA}>
+          Calculate GPA
+        </Button>
       </div>
 
-
+      <div className='center-title'>
+        {calculatedGPA !== null && (
+          <p style={{ marginTop: '10px' }}>
+            <strong>Calculated Major GPA:</strong> {calculatedGPA}
+          </p>
+        )}
+      </div>
+      <br/>
+      <br/>
+      <br/>
     </>
   )
 }
